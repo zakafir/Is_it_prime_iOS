e92168e9a39d805a99559204361c24122f39c5be
zakafir <zak.afir@gmail.com>
2017-10-13 00:55:58 +0100

    checking if a number is a prime

--- Is it Prime/ViewController.swift
+++ Is it Prime/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var numberField: UITextField!
+    
+    @IBOutlet weak var resultNumber: UILabel!
+    
+    @IBAction func submitButton(_ sender: Any) {
+        if let numberEnterredString = numberField.text {
+            
+            let numberEnterredInteger = Int(numberEnterredString)
+            
+            if let number = numberEnterredInteger {
+                
+                var isPrime = true
+                
+                var i = 2
+                
+                while i < number {
+                    
+                    if number % i == 0{
+                        isPrime = false
+                        break
+                    }
+                    i = i + 1
+                }
+                
+                if isPrime {
+                    resultNumber.text = "\(number) is a Prime"
+                } else {
+                    resultNumber.text = "\(number) is NOT a Prime !"
+                }
+                
+            } else {
+                resultNumber.text = "Please enter a positive whole number"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
